154ebfeb8b11751ec4913bec91ca492ad48f783d
Fasil <fasil17062002@gmail.com>
2022-04-02 22:23:19 +0530

    git ifnored

--- lib/configs/configs.dart
+++
@@ -1,6 +0,0 @@
-class Configs {
-  static const publishableKey =
-      "pk_test_51KjjA2SAYCwl4DFOPZWuqdcSUzqiuRsHGTvEXHtzzy5YHLsFXG9NWJKl48NmZYGijA5wwAcEdexzVorcVjEjsyvB00X1qlGKcy";
-  static const secretKey =
-      "sk_test_51KjjA2SAYCwl4DFODI1TcqI4RAf6DeupUeD28Wbp3S6YKi9vqNJE65Xab4Aqai41xlzX8ce6Njqw29TciD3okt1E00yzxHWauO";
-}
